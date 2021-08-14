bee80e959dc943a2ddb67b050ac63c99dd6c3af4
Sergey Kozlovskiy <sergey.kozlovskiy@jetbrains.com>
2021-08-14 17:30:54 +0300

    Structuring our application

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// App struct which contains pointers to database connections
+type App struct{}
+
+func (a *App) Run() error {
+	fmt.Println("Setting up our API")
+	return nil
+}
+
 func main() {
 	fmt.Println("Hello, GoLand")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
